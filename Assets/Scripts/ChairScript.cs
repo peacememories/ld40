962731f01e1248cde4163c39950d4a73c06625ad
Gabriel Pickl <peacememories@users.noreply.github.com>
2017-12-03 19:21:43 +0100

    Added seating feature.

--- Assets/Scripts/ChairScript.cs
+++ Assets/Scripts/ChairScript.cs
@@ -9,15 +9,33 @@ public class ChairScript : MonoBehaviour {
     private Sprite initialSprite;
 
     [SerializeField]
-    private Sprite seatedSprite;
+    private Sprite seatedSprite = null;
 
     [SerializeField]
     private List<GameObject> neighbors;
+
+    private Person Person = null;
+
+    private GameScript GlobalScript = null;
 
 	// Use this for initialization
 	void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
         initialSprite = spriteRenderer.sprite;
+
+        GameScript[] gameScripts = FindObjectsOfType<GameScript>();
+
+        if(gameScripts.Length >= 1)
+        {
+            GlobalScript = gameScripts[0];
+            if(gameScripts.Length > 1)
+            {
+                Debug.LogWarningFormat("Expected one GameScript in scene but found %d. Using the first one found.", gameScripts.Length);
+            }
+        } else
+        {
+            throw new System.Exception("No GameScript components found in scene");
+        }
 	}
 	
 	// Update is called once per frame
@@ -27,6 +45,12 @@ public class ChairScript : MonoBehaviour {
 
     void OnMouseDown()
     {
-        spriteRenderer.sprite = seatedSprite;
+        
+        if (GlobalScript.ToBeSeated.Count > 0)
+        {
+            Person p = GlobalScript.ToBeSeated.Dequeue();
+            spriteRenderer.sprite = seatedSprite;
+            GlobalScript.SeatedPeople.Add(p);
+        }
     }
 }
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -2,10 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Person {
+    private HashSet<Person> PersonLikes = new HashSet<Person>();
+    private HashSet<Person> PersonDislikes = new HashSet<Person>();
+    public readonly string Name;
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public int calculateScore(ICollection<Person> people) {
+        int score = 0;
+        foreach (Person person in people)
+        {
+            if (PersonLikes.Contains(person))
+            {
+                score = 1;
+            }
+            if (PersonDislikes.Contains(person))
+            {
+                return -2;
+            }
+        }
+        return score;
+    }
+}
+
 public class GameScript : MonoBehaviour {
 
     // Seated People : Array
-    // Unseated People : Queue / Graph
+    // Unseated People : Queue
+    [SerializeField]
+    private string[] FirstNames = { };
+
+    [SerializeField]
+    private string[] LastNames = { };
+
+    [SerializeField]
+    private float PersonDelay = 5;
+
+    [SerializeField]
+    private int MaximumQueued = 3;
+
+    public HashSet<Person> SeatedPeople = new HashSet<Person>();
+    public Queue<Person> ToBeSeated = new Queue<Person>();
+
+    private float timeRemaining = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +57,36 @@ public class GameScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float deltaTime = Time.deltaTime;
+        timeRemaining -= deltaTime;
+        while (timeRemaining <= 0 && PersonDelay > 0)
+        {
+            timeRemaining += PersonDelay;
+            PersonArrives();
+        }
 	}
+
+    void PersonArrives()
+    {
+        if(ToBeSeated.Count < MaximumQueued)
+        {
+            string firstName = FirstNames[Random.Range(0, FirstNames.Length)];
+            string lastName = LastNames[Random.Range(0, LastNames.Length)];
+            Person p = new Person(firstName + " " + lastName);
+
+            // Add liked and disliked people
+
+            ToBeSeated.Enqueue(p);
+            Debug.Log("Queued " + p.Name);
+        } else
+        {
+            PersonLeft();
+        }
+    }
+
+    //Deduct points for person leaving
+    void PersonLeft()
+    {
+
+    }
 }
