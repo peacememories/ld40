96690e75c973ff7a3e74bff81e01ca8764cbee33
Gabriel Pickl <peacememories@users.noreply.github.com>
2017-12-03 05:07:36 +0100

    Added some of the chair connections.

---
+++ Assets/Scripts/ChairScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairScript : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+
+    private Sprite initialSprite;
+
+    [SerializeField]
+    private Sprite seatedSprite;
+
+    [SerializeField]
+    private List<GameObject> neighbors;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        initialSprite = spriteRenderer.sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        spriteRenderer.sprite = seatedSprite;
+    }
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    // Seated People : Array
+    // Unseated People : Queue / Graph
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
